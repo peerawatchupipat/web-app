ff25e7fb5ac5b1fd3792fd581b2677d460af6b91
Peerawat.Chupipat <Peerawat.chupipat@kellogg.northwestern.edu>
2026-05-15 01:17:18 +0000

    Update contact site with company name

---
+++ contact_prompt.md
@@ -0,0 +1,11 @@
+! run test: "integration/contact_test.rb"
+! verify tests fail
+
+loop until tests pass do
+  Build a view that does this: 
+  
+    Loop through all contacts
+    Display:
+      Contacts' full name
+      Name of the contact's company
+end
--- test/integration/contacts_test.rb
+++ test/integration/contacts_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ContactsTest < ActionDispatch::IntegrationTest
-  test "contacts index displays contact names" do
+  test "contacts index displays contact names and company" do
     company = Company.create(name: "Apple")
     Contact.create(first_name: "Tim", last_name: "Cook", company_id: company.id)
     Contact.create(first_name: "Dario", last_name: "Amodei", company_id: company.id)
@@ -10,7 +10,7 @@ class ContactsTest < ActionDispatch::IntegrationTest
 
     assert_response :success
 
-    assert_select "li", text: "Tim Cook"
-    assert_select "li", text: "Dario Amodei"
+    assert_select "li", text: "Tim Cook - Apple"
+    assert_select "li", text: "Dario Amodei - Apple"
   end
 end
